f28620f25680df2363a3e8e9493980e833597a29
Silvio <silviopeixe@hotmail.com>
2019-03-19 11:05:43 -0300

    Megasena beckup inserido por silvio 19-03-2019

---
+++ src/main/java/com/bcopstein/App.java
@@ -0,0 +1,16 @@
+package com.bcopstein;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String[] resultado = ResultadoMegasena.obtemUltimoResultado();
+        for (String dezena: resultado) {
+              System.out.print(dezena + " ");
+        }
+    }
+}
